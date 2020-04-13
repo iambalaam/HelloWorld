adfe9a630cfcde0455c8d28e2ffe789b21451a7f
Guy <guy.balaam@gmail.com>
2020-04-13 12:10:34 +0100

    Hello World

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Something thing = new Something();
+            int number = thing.GetAnInt();
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Something.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class Something
+    {
+    public int GetAnInt()
+        {
+            return 2;
+        }
+    }
+}
